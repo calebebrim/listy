05c763395fa2f76841aaa8bf587f55d7f9607fad
Calebe Brim <calebebrim@gmail.com>
2017-08-24 21:25:53 -0300

    login facebook

--- server/main.js
+++ server/main.js
@@ -3,3 +3,14 @@ import { Meteor } from 'meteor/meteor';
 Meteor.startup(() => {
   // code to run on server at startup
 });
+
+
+ServiceConfiguration.configurations.remove({
+    service: "facebook"
+});
+
+ServiceConfiguration.configurations.insert({
+    service: "facebook",
+    appId: '343619416082919',
+    secret: 'c2e320a35ce9e3a28bd855675ac1e850'
+});
